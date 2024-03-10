5207808c78e7c1d65e1808b6a251ca3da5361f1d
Ivan Piskun <tembir.ivan@gmail.com>
2024-03-10 22:48:57 +0300

    type:fix - redirect

--- frontend/src/app/StoreProvider.tsx
+++ frontend/src/app/StoreProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { setToken } from "@/shared/lib/features/jwt/jwtSlice";
 import { AppStore, makeStore } from "@/shared/lib/store";
-import { usePathname, useRouter } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 import { useEffect, useRef } from "react";
 import { Provider } from "react-redux";
 
@@ -10,17 +10,20 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
   const pathName = usePathname();
   const storeRef = useRef<AppStore>();
+  const token = localStorage.getItem("token");
+
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
+  if (pathName === "/" && !token) redirect("/login");
+  if (token) storeRef.current!.dispatch(setToken(token));
 
   useEffect(() => {
     const token = localStorage.getItem("token");
 
-    if (pathName === "/" && !token) router.push("/login");
+    if (pathName === "/" && !token) redirect("/login");
     if (token) storeRef.current!.dispatch(setToken(token));
   }, [pathName]);
 
